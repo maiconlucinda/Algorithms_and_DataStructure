30ab6ed3975b8ee1d7f07e505c9e8ed79abc25aa
Maicon Lucinda <mayconlucindatj60@gmail.com>
2021-11-13 10:20:19 -0300

    Manupulando arquivos

---
+++ 22_manipulacao_de_arquivos_de_texto.py
@@ -0,0 +1,18 @@
+
+# Abrindo um arquivo de texto e realizando um for em cada linha to arquivo
+with open('frase1.txt') as tex:
+    for linha in tex:
+        print(linha)
+
+
+# Abrindo um arquivo de texto e adicionando cada linha do arquivo dentro de uma lista.
+with open('frase1.txt') as tex2:
+    for cadalinha in tex2:
+        r = tex2.readlines()
+print(r)
+
+
+# Criando um arquivo txt com a linguagem Python (necessário colocar o mode w, quer dizer write)
+with open('frase2.txt', 'w') as texto:
+    texto.write('Olá a todos')
+
