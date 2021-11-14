b021acd909596e382d11151fd93e31be25b976cc
Maicon Lucinda <mayconlucindatj60@gmail.com>
2021-11-14 09:22:39 -0300

    poo_praticando

---
+++ 27_orientacao_a_objetos_teoria.py
@@ -0,0 +1,10 @@
+
+# Uma classe é um agrupamento de objetos similares que apresentam os mesmos atributos e métodos.
+
+# Molde de bonecos de gesso
+    # Define o formato e tamanho
+
+    # O molde é sempre o mesmo, porém os objetos gerados podem ter características variadas, respeitando a estrutura
+    # básica do molde
+
+    
