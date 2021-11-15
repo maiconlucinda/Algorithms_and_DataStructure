ac2d89f34248367136d4886e79c8a9d8f014006a
Maicon Lucinda <mayconlucindatj60@gmail.com>
2021-11-15 08:16:09 -0300

    notacao BIG O

---
+++ 02_BIG-O_notation/01_notacao_BIG-O.py
@@ -0,0 +1,22 @@
+
+# O QUE É NOTAÇÃO BIG-O?
+    # Como comparar dois algoritmos desenvolvidos por diferentes programadores?
+    # Comparação objetiva entre algoritmos (não leva em conta sismema, nem linguagem... somente o algoritmo em si)
+    # Considera diferenças entre poder de processamento, sistema operacional,linguagem de programação
+    # O quanto a "complexidade" do algoritmo aumento de acordo com as entradas.
+
+
+# Função 1
+
+import timeit
+def soma1(n):
+    soma = 0
+    for i in range(n + 1):
+        soma += i
+    return soma
+print(soma1(10))
+
+
+# Usando o recurso %timeit é possível saber a quantidade de ações que uma determinada função precisa executar
+# para ser finalizada, caso seja realizada 9 ações por exemplo, podemos dizer que a função é O - 9.
+
