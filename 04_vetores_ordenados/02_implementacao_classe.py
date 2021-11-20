f89a6c1489dfa41fd215362bba0f037b541916b2
Maicon Lucinda <mayconlucindatj60@gmail.com>
2021-11-20 06:20:13 -0300

    vetor ordenado

--- 04_vetores_ordenados/02_implementacao_classe.py
+++ 04_vetores_ordenados/02_implementacao_classe.py
@@ -4,6 +4,7 @@ import numpy as np
 
 class VetorOrdenado:
 
+
     # O primeiro método precisa ser o construtor, esse irá definir as propriedades para o escopo.
     # Irá também criar a estrutura necessária
     def __init__(self, capacidade):
@@ -24,41 +25,56 @@ class VetorOrdenado:
     # 1 - Caso o valor do última posição seja igual a -1, posso retornar -1, para dizer que não existe ese valor dentro.
     # 2 - Loop para buscar até a última posição, algum valore que corresponda ao valor procurado
     # 3 - Caso o valor seja encontrado, retorne a posição do mesmo.
+    # Big-O = O(n)
     def pesquisa(self, valor):
-        if self.ultima_posicao == -1:
-            return self.ultima_posicao
-        else:
-            for i in range(self.ultima_posicao + 1):
-                if valor == self.valores[i]:
-                    return i
-            return -1
+        for i in range(self.ultima_posicao + 1):
+            if self.valores[i] > valor | i == self.ultima_posicao:
+                return -1
+            if self.valores[i] == valor:
+                return i
 
 
+    #1 -  Validar se a estrutura já está cheia.
+    #2 -  Encontrar posicao onde o elemento deverá ser adicionado
+    #3 -  Movendo uma posicao a frente, todos os elementos presentes no vetor
+    #4 -  Adicionar o novo valor na posicao correta do vetor
     # Big-O = O(n)
     def insercao(self, valor):
-
-        # Caso a estrutura estiver cheia
+        #1
         if self.ultima_posicao == self.capacidade - 1:
-            print('Vetor atingiu capacidade máxima')
-            return
+            print('Capacidade máxima atingida')
 
-        # Maneira de encontrar a posição onde o valor deverá ficar
+        #2
         posicao = 0
-        for i in range(self.ultima_posicao + 1):
+        for i in range(self.ultima_posicao +1):
             posicao = i
             if self.valores[i] > valor:
                 break
             if i == self.ultima_posicao:
                 posicao = i + 1
 
-        # Movendo os valores para um posicao a frente
+        #3
         x = self.ultima_posicao
         while x >= posicao:
             self.valores[x + 1] = self.valores[x]
             x -= 1
 
+        #4
         self.valores[posicao] = valor
         self.ultima_posicao += 1
+
+
+    #1 - Verificar se o valor existe dentro do vetor, caso não encontre, retorne somente uma mensagem
+    #2 - Caso exista o valor, tendo em mente sua posição, posso mover os elementos que estão a frente uma casa a esquerda
+    #3 - Decrementar o tamanho do vetor
+    def exclusao(self, valor):
+        posicao = self.pesquisa(valor)
+        if posicao == -1:
+            return -1
+        else:
+            for i in range(posicao, self.ultima_posicao):
+                self.valores[i] = self.ultima_posicao[i + 1]
+            self.ultima_posicao -= 1
 
 
 
@@ -66,9 +82,11 @@ vetor = VetorOrdenado(10)
 
 vetor.insercao(5)
 vetor.insercao(8)
-vetor.insercao(9)
+vetor.insercao(6)
 vetor.insercao(7)
 vetor.insercao(2)
+print(vetor.pesquisa(8))
+vetor.exclusao(8)
 
 vetor.imprimir()
 
