59f8ccc91b64bf6cb84c32e8bf7a85546f808f13
Maicon Lucinda <mayconlucindatj60@gmail.com>
2021-11-20 09:44:28 -0300

    debug

--- 04_vetores_ordenados/02_implementacao_classe.py
+++ 04_vetores_ordenados/02_implementacao_classe.py
@@ -2,7 +2,7 @@
 # importar tudos os módulos que serão necessário utilizar
 import numpy as np
 
-class VetorOrdenado:
+class OrderdedVector:
 
 
     # O primeiro método precisa ser o construtor, esse irá definir as propriedades para o escopo.
@@ -14,7 +14,7 @@ class VetorOrdenado:
         self.valores = np.empty(self.capacidade, dtype=int)
 
 
-    def imprimir(self):
+    def print(self):
         if self.ultima_posicao == -1:
             print('O vetor está vazio')
         else:
@@ -26,12 +26,14 @@ class VetorOrdenado:
     # 2 - Loop para buscar até a última posição, algum valore que corresponda ao valor procurado
     # 3 - Caso o valor seja encontrado, retorne a posição do mesmo.
     # Big-O = O(n)
-    def pesquisa(self, valor):
+    def search(self, valor):
         for i in range(self.ultima_posicao + 1):
             if self.valores[i] > valor | i == self.ultima_posicao:
                 return -1
             if self.valores[i] == valor:
                 return i
+        return -1
+
 
 
     #1 -  Validar se a estrutura já está cheia.
@@ -39,19 +41,25 @@ class VetorOrdenado:
     #3 -  Movendo uma posicao a frente, todos os elementos presentes no vetor
     #4 -  Adicionar o novo valor na posicao correta do vetor
     # Big-O = O(n)
-    def insercao(self, valor):
+    def insert(self, valor):
         #1
+        # Por que -1? Porque no Python o indice comeca no zero, então de zero até 9 temos 10 posições.
         if self.ultima_posicao == self.capacidade - 1:
             print('Capacidade máxima atingida')
 
         #2
         posicao = 0
+        # Por que +1? Porque o range se inicia com -1, então o +1 irá fazer com que chegue no zero. E depois sempre irá um número a frente do necessário
         for i in range(self.ultima_posicao +1):
-            posicao = i
-            if self.valores[i] > valor:
-                break
-            if i == self.ultima_posicao:
-                posicao = i + 1
+            if self.valores[i] == valor:
+                print(f'The value {valor} already exists in the vector')
+                return -1
+            else:
+                posicao = i
+                if self.valores[i] > valor:
+                    break
+                if i == self.ultima_posicao:
+                    posicao = i + 1
 
         #3
         x = self.ultima_posicao
@@ -67,28 +75,29 @@ class VetorOrdenado:
     #1 - Verificar se o valor existe dentro do vetor, caso não encontre, retorne somente uma mensagem
     #2 - Caso exista o valor, tendo em mente sua posição, posso mover os elementos que estão a frente uma casa a esquerda
     #3 - Decrementar o tamanho do vetor
-    def exclusao(self, valor):
-        posicao = self.pesquisa(valor)
+    def delete(self, valor):
+        posicao = self.search(valor)
         if posicao == -1:
             return -1
         else:
             for i in range(posicao, self.ultima_posicao):
-                self.valores[i] = self.ultima_posicao[i + 1]
+                self.valores[i] = self.valores[i + 1]
             self.ultima_posicao -= 1
 
 
 
-vetor = VetorOrdenado(10)
+vector = OrderdedVector(10)
 
-vetor.insercao(5)
-vetor.insercao(8)
-vetor.insercao(6)
-vetor.insercao(7)
-vetor.insercao(2)
-print(vetor.pesquisa(8))
-vetor.exclusao(8)
+vector.insert(5)
+vector.insert(5)
+vector.insert(6)
+vector.insert(7)
+vector.insert(2)
+print(vector.search(8))
+vector.delete(5)
 
-vetor.imprimir()
+
+vector.print()
 
 
 
--- testes.py
+++ testes.py
@@ -1,23 +1,7 @@
 
 import numpy as np
+valor = 10
 
-class VetorOrdenado:
-
-
-    def __init__(self, capacidade):
-        self.capacidade = capacidade
-        self.ultima_posicao = -1
-        self.valores = np.empty(self.capacidade, dtype=int)
-
-
-    def pesquisa(self, valor):
-        if self.ultima_posicao == -1:
-            return self.ultima_posicao
-        else:
-            for i in range(self.ultima_posicao + 1):
-                if valor == self.valores[i]:
-                    return i
-            return -1
-
-vetor = VetorOrdenado(8)
-print(vetor.pesquisa(3))
+print(range(0))
+for i in range(0):
+    print(i)
